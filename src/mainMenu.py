2f90e36e7e7296daf6a7d2bb05e0e9b85c528f4e
Ethan Bowen <legostarswars2006@gmail.com>
2025-07-27 12:00:47 -0400

    Update mainMenu.py
    Included the function to open the window for the Password List

--- src/mainMenu.py
+++ src/mainMenu.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
 from addPassword import passwordWindow
+from viewListApp import viewPasswords
 
 #This creates the main application window
 def mainMenu():
@@ -13,7 +14,10 @@ def mainMenu():
     def openPassWindow():
         newWindow1 = passwordWindow()
 
-    def exit_application():
+    def openPassView():
+        newWindow2 = viewPasswords()
+
+    def exitApplication():
         """Exit the application."""
         if messagebox.askyesno("Exit", "Are you sure you want to exit?"):
             mainRoot.destroy()
@@ -32,11 +36,11 @@ def mainMenu():
     btn_function2 = tk.Button(button_frame, text="Add Password", width=20, command=openPassWindow)
     btn_function2.pack(pady=5)
 
-    btn_function2 = tk.Button(button_frame, text="Password Manager", width=20, command=placeholder_function)
+    btn_function2 = tk.Button(button_frame, text="Password Manager", width=20, command=openPassView)
     btn_function2.pack(pady=5)
 
     #This adds an Exit button
-    btn_exit = tk.Button(button_frame, text="Exit", width=20, command=exit_application)
+    btn_exit = tk.Button(button_frame, text="Exit", width=20, command=exitApplication)
     btn_exit.pack(pady=5)
 
     #This runs the application
