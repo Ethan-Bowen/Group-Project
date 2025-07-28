1563ea2bb82df687f9d2df966e47d1435b00928e
Ethan Bowen <legostarswars2006@gmail.com>
2025-07-28 19:56:55 -0400

    Update viewListApp.py

--- src/viewListApp.py
+++ src/viewListApp.py
@@ -10,7 +10,7 @@ class viewPasswords:
 
         #The loadList function should go here. Be sure to include an except
         #function if the file isn't found
-    def loadList():
+    def loadList(self, listbox=None):
         hashMap = HashMap()
         try:
             hashMap.readSavedPasswords("passwords.txt")
@@ -18,13 +18,14 @@ class viewPasswords:
             messagebox.showerror("Password File not Found", "Password File Missing!")
             return()
         counter = 1
-        for key in hashMap.map:
-            listbox.insert(counter, key, hashMap.getValue(key))
+        for key in hashMap.map():
+            listbox.insert(tk.END, counter, key, hashMap.getValue(key))
             counter += 1
 
         # This is the Listbox widget
         listbox = tk.Listbox(root, height=15, width=50)
         listbox.pack(pady=10)
+        
 
 if __name__ == "__main__":
     root = tk.Tk()
