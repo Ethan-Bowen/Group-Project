7c47919c21b4fda4e7012dd86008cdedc15384dd
Ethan Bowen <legostarswars2006@gmail.com>
2025-07-28 20:36:11 -0400

    Update viewListApp.py

--- src/viewListApp.py
+++ src/viewListApp.py
@@ -2,35 +2,28 @@ import tkinter as tk
 from tkinter import messagebox
 from passwordBank import HashMap
 
-class viewPasswords:
-    def __init__(self, root):
-        self.root = root
-        self.window = tk.Toplevel()
-        self.window.title("View Passwords")
-        self.window.geometry("300x200")
+def viewPasswords():
+    viewWindow = tk.Toplevel()
+    viewWindow.title("View Passwords")
+    viewWindow.geometry("300x200")
 
+    listbox = tk.Listbox(viewWindow)
+    listbox.pack(pady=10, padx=10)
         #The loadList function should go here. Be sure to include an except
         #function if the file isn't found
-        def loadList():
+    def loadList():
 
-            listbox = tk.Listbox(root, height=15, width=50)
-            listbox.pack(pady=10, padx=10)
-            hashMap = HashMap()
-            try:
-                hashMap.readSavedPasswords("passwords.txt")
-            except FileNotFoundError:
-                messagebox.showerror("Password File not Found", "Password File Missing!")
-                return()
-            counter = 1
-            for key in hashMap.map():
-                listbox.insert(tk.END, counter, key, hashMap.getValue(key))
-                counter += 1
-                return()
 
-        listButton = tk.Button(root, text="Show List", command=loadList)
-        listButton.pack(pady=20)
+        hashMap = HashMap()
+        try:
+            hashMap.readSavedPasswords("passwords.txt")
+        except FileNotFoundError:
+            messagebox.showerror("Password File not Found", "Password File Missing!")
+            return()
+        counter = 1
+        for key in hashMap.map():
+            listbox.insert(tk.END, counter, key, hashMap.getValue(key))
+            counter += 1
+            return()
 
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = viewPasswords(root)
-    app.root.mainloop()
+    loadList()
