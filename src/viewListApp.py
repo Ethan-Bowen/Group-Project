3a049af0909a48ccfd11e571c352346ee922dd93
Brandon Springer <Brandonspring727@gmail.com>
2025-07-28 21:00:11 -0400

    commit

--- src/viewListApp.py
+++ src/viewListApp.py
@@ -2,28 +2,27 @@ import tkinter as tk
 from tkinter import messagebox
 from passwordBank import HashMap
 
+listbox = None
 def viewPasswords():
     viewWindow = tk.Toplevel()
     viewWindow.title("View Passwords")
     viewWindow.geometry("300x200")
 
-    listbox = tk.Listbox(viewWindow)
-    listbox.pack(pady=10, padx=10)
         #The loadList function should go here. Be sure to include an except
         #function if the file isn't found
-    def loadList():
+# def loadList():
+    listbox = tk.Listbox(viewWindow, width=300, height=200)
+    listbox.pack(pady=10, padx=10, expand=True)
+    hashMap = HashMap()
+    try:
+        hashMap.readSavedPasswords("passwords.txt")
+    except FileNotFoundError:
+        messagebox.showerror("Password File not Found", "Password File Missing!")
+        return()
+    counter = 1
+    for key in hashMap.map:
+        listbox.insert(tk.END, key + "    " + hashMap.getValue(key))
+        counter += 1
+        return()
 
-
-        hashMap = HashMap()
-        try:
-            hashMap.readSavedPasswords("passwords.txt")
-        except FileNotFoundError:
-            messagebox.showerror("Password File not Found", "Password File Missing!")
-            return()
-        counter = 1
-        for key in hashMap.map():
-            listbox.insert(tk.END, counter, key, hashMap.getValue(key))
-            counter += 1
-            return()
-
-    loadList()
+# loadList()
