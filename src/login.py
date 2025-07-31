137cf463ab712b897229f78e738779c6cbd1a274
Brandon Springer <Brandonspring727@gmail.com>
2025-07-31 19:01:41 -0400

    More comments

--- src/login.py
+++ src/login.py
@@ -29,41 +29,41 @@ class UserCreatorGUI:
     #Used for the create user button
     #Uses the create user fn in user to make a new folder, salt file, and encryption key
     def createUser(self):
-        username = self.usernameEntry.get().strip()
-        password = self.passwordEntry.get().strip()
+        username = self.usernameEntry.get().strip() #Gets username from entry box
+        password = self.passwordEntry.get().strip() #Gets password from entry box
         #if username or password is left blank
         if not username or not password:
-            messagebox.showerror("Error", "Both fields are required!")
+            messagebox.showerror("Error", "Both fields are required!") #Error if fields are left blank
             return
         #If both fields are entered then It creates a new user
         else:
-            User.createUser(username=username, password=password)
+            User.createUser(username=username, password=password) #Creates user using the user fn and shows successful message box when finished
             messagebox.showinfo("Success", f"User '{username}' created successfully!")
             self.openMainWindow()
 
     #Used for the login button
     #Uses the decryption fn from cryption to login a user if they have the correct password
     def login(self):
-        username = self.usernameEntry.get().strip()
-        password = self.passwordEntry.get().strip()
+        username = self.usernameEntry.get().strip() #Gets username from entry box
+        password = self.passwordEntry.get().strip() #Gets password from entry box
 
         #if username or password is left blank
         if not username or not password:
-            messagebox.showerror("Error", "Both fields are required!")
+            messagebox.showerror("Error", "Both fields are required!") #Error if fields are left blank
             return
-        #checks if the user exists
+        #checks if the user exists already
         elif not os.path.isdir(username):
             print()
             print(os.getcwd())
-            messagebox.showerror("Error", "Invalid Username!")
+            messagebox.showerror("Error", "Invalid Username!") #Opens an error message box if the user exists already
             return
         #Decryption with correct password
         else:
             os.chdir(username)
-            key = cryption.genarateKey(password, loadSalt=True)
-            try:
+            key = cryption.genarateKey(password, loadSalt=True) #Generates a key using the salt file and given password
+            try: #Tries to decrypt the file using the generated key
                 cryption.decrypt("passwords.txt", key)
-            except cryptography.fernet.InvalidToken:
+            except cryptography.fernet.InvalidToken: #If the key doesn't match the saved key then it will return an error message box
                 messagebox.showerror("Error", "Incorrect Password!")
                 os.chdir("..")
                 return()
@@ -76,7 +76,7 @@ class UserCreatorGUI:
         self.root.destroy()
         mainMenu()
 
-
+#Used to run the program
 if __name__ == "__main__":
     root = tk.Tk()
     app = UserCreatorGUI(root)
